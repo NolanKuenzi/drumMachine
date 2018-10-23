acc1927cf1e25e28d36b5d460f242638bdab3c7f
Nolan Kuenzi <mr.nolank@gmail.com>
2018-10-22 21:55:11 -0700

    Layout update

--- src/index.js
+++ src/index.js
@@ -4,19 +4,18 @@ import './index.css';
 import registerServiceWorker from './registerServiceWorker';
 import { DrumPad } from './drumPad.js';
 
-
 class App extends React.Component {
-	constructor(props) {
-		super(props);
-	}
-	render() {
-		return (
-			<div id="drum-machine">
-				<h1 id="header">Drum Machine</h1>				
-				<DrumPad />
-			</div>
-		);
-	}
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div id="drum-machine">
+        <h1 id="header">Drum Machine</h1>				
+        <DrumPad />
+      </div>
+    );
+  }
 }
 
 ReactDOM.render(<App />, document.getElementById('root'));
