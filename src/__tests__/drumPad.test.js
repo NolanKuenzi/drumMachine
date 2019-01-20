2866a6dc3bac03dbb60810c1131ab398affc5426
Nolan Kuenzi <mr.nolank@gmail.com>
2019-01-19 19:43:07 -0800

    testing revised

--- src/__tests__/drumPad.test.js
+++ src/__tests__/drumPad.test.js
@@ -1,10 +1,39 @@
 import React from 'react';
-import Enzyme, { shallow, mount } from 'enzyme';
 import { DrumPad } from '../drumPad.js';
+import { cleanup, render, fireEvent } from 'react-testing-library';
 
-describe('<DrumPad /> Component', () => {
-  it('Renders a <div />', () => {
-    const wrapper = shallow(<DrumPad />);
-    expect(wrapper.exists()).toBe(true);
+afterEach(cleanup);
+
+const playAudioTest = jest.spyOn(DrumPad.prototype, "playAudio");
+describe("<DrumPad /> Component", () => {
+  test("Click events change the display text, the drumPad's item's opacity, and play the audio", () => {
+    const { getByTestId } = render(<DrumPad />);
+    const displayText = getByTestId("displayValue");
+    const clickTest1 = getByTestId("Heater-1");
+    const clickTest2 = getByTestId("Chord_1");
+    fireEvent.click(clickTest1);
+    expect(displayText.value).toBe("Heater-1");
+    expect(clickTest1.style.opacity).toBe("0.1");
+    expect(playAudioTest).toHaveBeenCalled();
+    fireEvent.click(clickTest2);
+    expect(displayText.value).toBe("Chord_1");
+    expect(clickTest2.style.opacity).toBe("0.1");
+    expect(playAudioTest).toHaveBeenCalled();
   });
-});
+  test("keyUp events change the display text, the drumPad's item's opacity, and play the audio", () => {
+    const { getByTestId } = render(<DrumPad />);
+    const keyEventArea = getByTestId("drumPadContain");
+    const displayText = getByTestId("displayValue");
+    const opacityTest1 = getByTestId("Heater-1");
+    const opacityTest2 = getByTestId("Chord_1");
+    fireEvent.keyUp(keyEventArea, {key: 'q'});
+    expect(displayText.value).toBe("Heater-1");
+    expect(playAudioTest).toHaveBeenCalled();
+    expect(opacityTest1.style.opacity).toBe("0.1");
+    fireEvent.keyUp(keyEventArea, {key: 's'});
+    expect(displayText.value).toBe("Chord_1");
+    expect(opacityTest2.style.opacity).toBe("0.1");
+    expect(playAudioTest).toHaveBeenCalled();
+  });
+}); 
+ 
--- src/drumPad.js
+++ src/drumPad.js
@@ -20,7 +20,6 @@ class DrumPad extends React.Component {
     document.removeEventListener("click", this.playFunction, false);
   }
   playFunction(event) {
-    console.log(typeof event.target.id);
     if (event.key === undefined) {
       this.playAudio(event.target.lastChild.id);
     } else {
@@ -45,13 +44,13 @@ class DrumPad extends React.Component {
   }
   render() {
     return (
-      <div id="drumPadContain">
-        <div id="display"><input type="text" value={this.state.audioName}></input></div>	
+      <div id="drumPadContain" data-testid="drumPadContain">
+        <div id="display"><input type="text" data-testid="displayValue" value={this.state.audioName}></input></div>	
           <div id="drumKeys">	
             
             <div className="row">
-              <div className="drum-pad" id="Heater-1"  tabIndex="1"><h1>Q</h1>
-                <audio className="clip" id="q"><source src="https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3" type="audio/mp3" /></audio>
+              <div className="drum-pad" id="Heater-1" data-testid="Heater-1" tabIndex="1"><h1>Q</h1>
+                <audio className="clip" id="q"><source src="https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3" type="audio/mp3" data-testid="audio1" /></audio>
 	            </div>		
               <div className="drum-pad" id="Dsc_Oh"><h1>W</h1>
                 <audio className="clip" id="w"><source src="https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3" type="audio/mp3" /></audio>
@@ -65,8 +64,8 @@ class DrumPad extends React.Component {
               <div className="drum-pad" id="side_stick_1"><h1>A</h1>
                 <audio className="clip" id="a"><source src="https://s3.amazonaws.com/freecodecamp/drums/side_stick_1.mp3" type="audio/mp3" /></audio>
               </div>
-              <div className="drum-pad" id="Chord_1"><h1>S</h1>
-                <audio className="clip" id="s"><source src="https://s3.amazonaws.com/freecodecamp/drums/Chord_1.mp3" type="audio/mp3" /></audio>
+              <div className="drum-pad" id="Chord_1" data-testid="Chord_1"><h1>S</h1>
+                <audio className="clip" id="s"><source src="https://s3.amazonaws.com/freecodecamp/drums/Chord_1.mp3" type="audio/mp3" data-testid="audio2" /></audio>
               </div>
               <div className="drum-pad" id="RP4_KICK_1"><h1>D</h1>
                 <audio className="clip" id="d"><source src="https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3" type="audio/mp3" /></audio>
